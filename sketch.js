f7d50f57751585eb6b6c9839fdd4df724006fb9a
MrSpiritualMomo <76740889+MrSpiritualMomo@users.noreply.github.com>
2021-01-12 20:44:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var fixedRect,movingRect;
+function setup() {
+  createCanvas(800,400);
+  fixedRect= createSprite(200, 200, 50, 80);
+  movingRect= createSprite(400, 200, 80, 30);
+
+}
+
+function draw() {
+  background(255,255,255);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 )
+  {movingRect.shapeColor="red";
+  fixedRect.shapeColor="red";
+
+}
+else{movingRect.shapeColor="green";
+fixedRect.shapeColor="green";
+
+}
+  drawSprites();
+}
